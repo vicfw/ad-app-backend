34c093f30c3c78d5711cdcd1353982a1f7fd9775
Farid <faridforward222@gmail.com>
2022-11-30 11:44:57 +0330

    ss

--- controller/uploadController.js
+++ controller/uploadController.js
@@ -1,24 +1,26 @@
-const catchAsync = require("../utils/catchAsync");
+const catchAsync = require('../utils/catchAsync');
 
 exports.categoryImage = catchAsync(async (req, res, next) => {
   if (!req.file.fieldname) {
-    return res.status(400).json({ status: "fail", message: "Enter a File!" });
+    return res.status(400).json({ status: 'fail', message: 'Enter a File!' });
   }
 
   res.status(200).json({
-    status: "success",
-    message: "uploaded successfully",
+    status: 'success',
+    message: 'uploaded successfully',
     data: req.file.Location,
   });
 });
 exports.adImage = catchAsync(async (req, res, next) => {
   if (!req.file.fieldname) {
-    return res.status(400).json({ status: "fail", message: "Enter a File!" });
+    return res.status(400).json({ status: 'fail', message: 'Enter a File!' });
   }
 
+  console.log(req.file);
+
   res.status(200).json({
-    status: "success",
-    message: "uploaded successfully",
+    status: 'success',
+    message: 'uploaded successfully',
     data: req.file.Location,
   });
 });
