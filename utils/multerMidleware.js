dda426361d9e9093cb945954603564514df1b17d
Farid <faridforward222@gmail.com>
2022-12-02 16:23:41 +0330

    size changed in uploaded xs size

--- utils/multerMidleware.js
+++ utils/multerMidleware.js
@@ -1,9 +1,9 @@
-const multer = require("multer");
-const aws = require("aws-sdk");
-const s3Storage = require("multer-sharp-s3");
+const multer = require('multer');
+const aws = require('aws-sdk');
+const s3Storage = require('multer-sharp-s3');
 const s3 = new aws.S3({});
 
-console.log(process.env.S3_UPLOAD_SECRET, "process.env.S3_UPLOAD_SECRET");
+console.log(process.env.S3_UPLOAD_SECRET, 'process.env.S3_UPLOAD_SECRET');
 
 s3.config.update({
   secretAccessKey: process.env.S3_UPLOAD_SECRET, // Not working key, Your SECRET ACCESS KEY from AWS should go here, never share it!!!
@@ -13,8 +13,8 @@ s3.config.update({
 
 const storage2 = s3Storage({
   s3,
-  Bucket: "adsphoto",
-  ACL: "",
+  Bucket: 'adsphoto',
+  ACL: '',
   Key: `${process.env.S3_UPLOAD_BUCKET}/categoryImages/${Date.now()}`,
   resize: {
     width: 60,
@@ -25,15 +25,15 @@ const storage2 = s3Storage({
 
 const adPhotoStorage = s3Storage({
   s3,
-  Bucket: "adsphoto",
-  ACL: "",
+  Bucket: 'adsphoto',
+  ACL: '',
   Key: `${process.env.S3_UPLOAD_BUCKET}/adImages/${Date.now()}`,
   resize: [
     // { suffix: 'xlg', width: 1200, height: 1200 },
     // { suffix: 'lg', width: 800, height: 800 },
     // { suffix: 'md', width: 500, height: 500 },
     // { suffix: 'sm', width: 300, height: 300 },
-    { suffix: "xs", width: 140, height: 140 },
+    { suffix: 'xs', width: 140, height: 120 },
   ],
   max: true,
   multiple: true,
