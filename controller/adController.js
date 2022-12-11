cb599467c9f4a6c14e6cce2a46453c97eaa6f24f
Farid <faridforward222@gmail.com>
2022-12-11 22:01:49 +0330

    ss

--- controller/adController.js
+++ controller/adController.js
@@ -1,19 +1,19 @@
-const catchAsync = require("../utils/catchAsync");
-const Ad = require("../models/adModel");
+const catchAsync = require('../utils/catchAsync');
+const Ad = require('../models/adModel');
 exports.createAd = catchAsync(async (req, res, next) => {
   const ad = await Ad.create({ ...req.body, creator: req.user._id });
 
-  res.status(201).json({ status: "success", ad });
+  res.status(201).json({ status: 'success', ad });
 });
 
 exports.getAllAds = catchAsync(async (req, res, next) => {
   const query = req.query;
   const ads = await Ad.find({})
-    .populate({ path: "creator", populate: { path: "featuredAds" } })
+    .populate({ path: 'creator', populate: { path: 'featuredAds' } })
     .limit(query?.limit ? +query.limit : 9999)
     .skip(query?.page === 1 ? +query.limit : +query.page * +query.limit)
     .sort({ createdAt: -1 });
 
   const count = await Ad.estimatedDocumentCount();
-  res.status(201).json({ status: "success", count, ads });
+  res.status(201).json({ status: 'success', count, ads });
 });
