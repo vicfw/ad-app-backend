591b6fdabd9c79d3a754027c4d89eaf0d3e163c8
farid <faridforward222@gmail.com>
2022-12-17 12:24:44 +0330

    pagination done in getcurrent user ads field

--- controller/adController.js
+++ controller/adController.js
@@ -1,21 +1,21 @@
-const catchAsync = require('../utils/catchAsync');
-const Ad = require('../models/adModel');
+const catchAsync = require("../utils/catchAsync");
+const Ad = require("../models/adModel");
 exports.createAd = catchAsync(async (req, res, next) => {
   const ad = await Ad.create({ ...req.body, creator: req.user._id });
 
-  res.status(201).json({ status: 'success', ad });
+  res.status(201).json({ status: "success", ad });
 });
 
 exports.getAllAds = catchAsync(async (req, res, next) => {
   const query = req.query;
   const ads = await Ad.find({})
-    .populate({ path: 'creator', populate: { path: 'featuredAds' } })
-    .limit(query?.limit ? +query.limit : 9999)
+    .populate({ path: "creator", populate: { path: "featuredAds" } })
+    .limit(query?.limit ? +query.limit : 0)
     .skip(query?.page === 1 ? +query.limit : +query.page * +query.limit)
     .sort({ createdAt: -1 });
 
   const count = await Ad.estimatedDocumentCount();
-  res.status(201).json({ status: 'success', count, ads });
+  res.status(201).json({ status: "success", count, ads });
 });
 
 exports.getSingleAdController = catchAsync(async (req, res) => {
@@ -26,7 +26,7 @@ exports.getSingleAdController = catchAsync(async (req, res) => {
   const ad = await Ad.findOne({ _id });
 
   if (!ad) {
-    res.status(400).json({ status: 'fail', message: 'bad request' });
+    res.status(400).json({ status: "fail", message: "bad request" });
   }
-  res.status(200).json({ status: 'success', ad });
+  res.status(200).json({ status: "success", ad });
 });
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,38 +1,38 @@
-const express = require('express');
-const userController = require('./../controller/userController');
-const authController = require('./../controller/authController');
+const express = require("express");
+const userController = require("./../controller/userController");
+const authController = require("./../controller/authController");
 
 const router = express.Router();
 
-router.post('/signup', authController.signup);
-router.post('/login', authController.login);
-router.get('/logout', authController.logout);
+router.post("/signup", authController.signup);
+router.post("/login", authController.login);
+router.get("/logout", authController.logout);
 
-router.post('/forgotPassword', authController.forgotPassword);
-router.patch('/resetPassword/:token', authController.resetPassword);
+router.post("/forgotPassword", authController.forgotPassword);
+router.patch("/resetPassword/:token", authController.resetPassword);
 
 // Protect all routes after this middleware
 router.use(authController.protect);
 
-router.patch('/updateMyPassword', authController.updatePassword);
-router.get('/me', userController.getMe, userController.getUser);
+router.patch("/updateMyPassword", authController.updatePassword);
+router.get("/me", authController.protect, userController.getCurrentUser);
 router.patch(
-  '/updateMe',
+  "/updateMe",
   userController.uploadUserPhoto,
   userController.resizeUserPhoto,
   userController.updateMe
 );
-router.delete('/deleteMe', userController.deleteMe);
+router.delete("/deleteMe", userController.deleteMe);
 
 // router.use(authController.restrictTo('admin'));
 
 router
-  .route('/')
+  .route("/")
   .get(userController.getAllUsers)
   .post(userController.createUser);
 
 router
-  .route('/:id')
+  .route("/:id")
   .get(userController.getUser)
   .patch(userController.updateUser)
   .delete(userController.deleteUser);
