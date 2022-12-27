c577a9d60b923367d00fbf95ae0493a49fa150b6
farid <faridforward222@gmail.com>
2022-12-28 00:52:29 +0330

    ss

--- controller/adController.js
+++ controller/adController.js
@@ -1,47 +1,51 @@
-const catchAsync = require('../utils/catchAsync');
-const Ad = require('../models/adModel');
-const FeatureAd = require('../models/featuredAd');
+const catchAsync = require("../utils/catchAsync");
+const Ad = require("../models/adModel");
+const FeatureAd = require("../models/featuredAd");
 exports.createAd = catchAsync(async (req, res, next) => {
   const ad = await Ad.create({ ...req.body, creator: req.user._id });
 
-  res.status(201).json({ status: 'success', ad });
+  res.status(201).json({ status: "success", ad });
 });
 
 exports.updateAd = catchAsync(async (req, res, next) => {
+  console.log(req.params, "req.params");
+  console.log(req.body, "req.body");
   const ad = await Ad.findByIdAndUpdate(
-    { _id: req.body._id },
+    { _id: req.params.id },
     { ...req.body },
     { new: true, runValidators: true }
   );
 
-  res.status(201).json({ status: 'success', ad });
+  console.log(ad, "ad after update");
+
+  res.status(201).json({ status: "success", ad });
 });
 
 exports.getAllAds = catchAsync(async (req, res, next) => {
   const query = req.query;
   const ads = await Ad.find({})
-    .populate({ path: 'creator', populate: { path: 'featuredAds' } })
+    .populate({ path: "creator", populate: { path: "featuredAds" } })
     .limit(query?.limit ? +query.limit : 0)
     .skip(query?.page === 1 ? +query.limit : +query.page * +query.limit)
     .sort({ createdAt: -1 });
 
   const count = await Ad.estimatedDocumentCount();
-  res.status(201).json({ status: 'success', count, ads });
+  res.status(201).json({ status: "success", count, ads });
 });
 
 exports.getSingleAdController = catchAsync(async (req, res) => {
   const _id = req.params.id;
 
-  console.log(req.params, 'params');
+  console.log(req.params, "params");
 
   const ad = await Ad.findOne({ _id });
 
   console.log(ad);
 
   if (!ad) {
-    return res.status(400).json({ status: 'fail', message: 'bad request' });
+    return res.status(400).json({ status: "fail", message: "bad request" });
   }
-  return res.status(200).json({ status: 'success', ad });
+  return res.status(200).json({ status: "success", ad });
 });
 
 exports.deleteAd = catchAsync(async (req, res, next) => {
@@ -50,13 +54,13 @@ exports.deleteAd = catchAsync(async (req, res, next) => {
   await FeatureAd.findOneAndDelete({ ad: req.params.id });
 
   if (!doc) {
-    return next(new AppError('No document found with that ID', 404));
+    return next(new AppError("No document found with that ID", 404));
   }
 
-  console.log(doc, 'doc');
+  console.log(doc, "doc");
 
   return res.status(204).json({
-    status: 'success',
+    status: "success",
     data: null,
   });
 });
--- routes/adRoutes.js
+++ routes/adRoutes.js
@@ -1,19 +1,19 @@
-const express = require('express');
+const express = require("express");
 const {
   createAd,
   getAllAds,
   getSingleAdController,
   deleteAd,
   updateAd,
-} = require('../controller/adController');
-const { protect } = require('../controller/authController');
+} = require("../controller/adController");
+const { protect } = require("../controller/authController");
 const router = express.Router();
 
-router.post('/create', protect, createAd);
-router.patch('/', protect, updateAd);
-router.get('/', protect, getAllAds);
-router.get('/:id', getSingleAdController);
+router.post("/create", protect, createAd);
+router.patch("/:id", protect, updateAd);
+router.get("/", protect, getAllAds);
+router.get("/:id", getSingleAdController);
 
-router.delete('/:id', protect, deleteAd);
+router.delete("/:id", protect, deleteAd);
 
 module.exports = router;
