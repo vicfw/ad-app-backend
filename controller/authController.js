98b753a7b4130c72f3d722a12f78a84a160f58a8
Farid <faridforward222@gmail.com>
2023-02-15 01:12:56 +0330

    ss

--- controller/authController.js
+++ controller/authController.js
@@ -15,14 +15,14 @@ const signToken = (id) => {
 const createSendToken = (user, statusCode, req, res) => {
   const token = signToken(user._id);
 
-  res.cookie('jwt', token, {
-    path: '/',
-    maxAge: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-    ),
-    httpOnly: true,
-    secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
-  });
+  // res.cookie('jwt', token, {
+  //   path: '/',
+  //   maxAge: new Date(
+  //     Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+  //   ),
+  //   httpOnly: true,
+  //   secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
+  // });
 
   // Remove password from output
   user.password = undefined;
