e1dce87cc6d87101b5a19d7f03f2500f23730e0f
Farid <faridforward222@gmail.com>
2023-03-25 12:44:22 +0430

    hhtps socket

--- server.js
+++ server.js
@@ -9,9 +9,7 @@ process.on('uncaughtException', (err) => {
 
 const app = require('./app.js');
 
-const http = require(`${
-  process.env.NODE_ENV === 'development' ? 'http' : 'https'
-}`).Server(app);
+const http = require('https').Server(app);
 
 const socketIO = require('socket.io')(http, {
   pingTimeout: 60000,
