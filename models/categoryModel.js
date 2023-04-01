06db0a233fbf6730e549d8ee91bece8605e07682
Farid <faridforward222@gmail.com>
2023-04-01 13:41:10 +0430

    chenges in category model

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -6,7 +6,6 @@ const categorySchema = mongoose.Schema(
       type: String,
       required: true,
       trim: true,
-      lowercase: true,
       unique: true,
     },
     categoryImage: { type: String, default: 'pic' },
