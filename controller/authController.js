cfff526445a7a7dc9154ea18bcbe0289b362cf6b
Farid <faridforward222@gmail.com>
2023-04-19 17:20:53 +0430

    update

--- controller/authController.js
+++ controller/authController.js
@@ -166,11 +166,8 @@ exports.me = async (req, res, next) => {
 };
 
 exports.logout = (req, res) => {
-  res.cookie('jwt', 'loggedout', {
-    expires: new Date(Date.now() + 10 * 1000),
-    httpOnly: true,
-  });
-  // delete req.user;
+  res.clearCookie('jwt');
+  delete req.user;
   res.status(200).json({ status: 'success' });
 };
 
