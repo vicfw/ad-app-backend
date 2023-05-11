b240bfa4b17cdde4e5a4b5cf1c352eeeeeeee858
Farid <faridforward222@gmail.com>
2023-05-11 14:05:02 +0330

    chat deletion route

--- controller/chatController.js
+++ controller/chatController.js
@@ -1,6 +1,9 @@
 const catchAsync = require('../utils/catchAsync');
 const Chat = require('../models/chatModel');
 const User = require('../models/userModel');
+const factory = require('./handlerFactory');
+const Message = require('../models/messageModel');
+
 
 exports.accessChat = catchAsync(async (req, res) => {
   const { userId, adId } = req.body;
@@ -63,4 +66,18 @@ exports.fetchChats = catchAsync(async (req, res) => {
       });
       res.status(200).send(results);
     });
+});
+
+exports.deleteChat =  catchAsync(async (req, res, next) => {
+  const doc = await Chat.findByIdAndDelete(req.params.id);
+  await Message.remove({chat:req.params.id})
+
+  if (!doc) {
+    return next(new AppError('No document found with that ID', 404));
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
 });
--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { protect } = require('../controller/authController');
-const { accessChat, fetchChats } = require('../controller/chatController');
+const { accessChat, fetchChats, deleteChat } = require('../controller/chatController');
 
 const router = express.Router();
 
 router.route('/').post(protect, accessChat);
 router.route('/').get(protect, fetchChats);
+router.route('/:id').delete(protect, deleteChat);
 
 module.exports = router;
