be31a1ead8a9387702465044ed113f1886a7eba4
Farid <faridforward222@gmail.com>
2023-05-28 17:44:06 +0330

    test

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const { protect } = require('../controller/authController');
+const express = require("express");
+const { protect } = require("../controller/authController");
 const router = express.Router();
 const {
   categoryImage,
   adImage,
   profilePhoto,
-} = require('../controller/uploadController');
+} = require("../controller/uploadController");
 const {
   upload,
   adImageUpload,
   profilePhotoUpload,
-} = require('../utils/multerMidleware');
+} = require("../utils/multerMidleware");
 
-router.post('/categoryImage', protect, upload.single('image'), categoryImage);
-router.post('/adImage', adImageUpload.single('image'), adImage);
-router.post('/profilePhoto', profilePhotoUpload.single('image'), profilePhoto);
+router.post("/categoryImage", protect, upload.single("image"), categoryImage);
+router.post("/adImage", adImageUpload.single("image"), adImage);
+router.post("/profilePhoto", profilePhotoUpload.single("image"), profilePhoto);
 
 module.exports = router;
