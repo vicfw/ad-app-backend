dff9d01052406072955988a24779bf114c25451e
Farid <faridforward222@gmail.com>
2023-09-10 18:45:27 +0330

    changes for brand name

--- controller/authController.js
+++ controller/authController.js
@@ -274,7 +274,6 @@ exports.restrictTo = (...roles) => {
 
 exports.forgotPassword = catchAsync(async (req, res, next) => {
   // 1) Get user based on POSTed email
-
   const user = await User.findOne({ email: req.body.email });
   if (!user) {
     return next(new AppError("There is no user with email address.", 404));
