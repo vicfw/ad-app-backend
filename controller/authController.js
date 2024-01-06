2c9937e9f2bb07db1d0d7152330aac81a44fd3dd
Farid <faridforward222@gmail.com>
2024-01-06 14:34:23 +0330

    changed cookie domian

--- controller/authController.js
+++ controller/authController.js
@@ -28,7 +28,7 @@ const createSendToken = (user, statusCode, req, res, hasCookie) => {
 
       httpOnly: true,
       sameSite: "none",
-      domain: isSecure ? ".gettruck.ca" : undefined,
+      domain: isSecure ? ".api.gettruckloan.com" : undefined,
       secure: isSecure,
     });
   }
