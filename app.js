cceb779f416ebc9ee53deba373f7aa452aa30c11
Farid <faridforward222@gmail.com>
2024-01-14 01:07:45 +0330

    remove limit on json

--- app.js
+++ app.js
@@ -41,8 +41,8 @@ const limiter = rateLimit({
 });
 // app.use('/api', limiter);
 
-app.use(express.json({ limit: "10kb" }));
-app.use(express.urlencoded({ extended: true, limit: "10kb" }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 
