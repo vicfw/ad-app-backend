59bd69c7074dde2a4652d57dfd98f5d37e060743
Farid <faridforward222@gmail.com>
2024-01-14 15:24:27 +0330

    ss

--- controller/authController.js
+++ controller/authController.js
@@ -22,6 +22,7 @@ const createSendToken = (user, statusCode, req, res, hasCookie) => {
       expires: new Date(
         Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
       ),
+
       httpOnly: true,
       sameSite: "lax",
       domain: isSecure ? ".gettruck.ca" : undefined,
